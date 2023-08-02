9185a438be65e01c7345b1324aa569cdc45609f3
Sven Lindstroem <sven.lindstroem@gmail.com>
2023-08-02 14:13:17 +0200

    adding scan result component

---
+++ src/lib/ScanResult.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { appstate } from '$lib/store'
+  export let scanResult: any
+</script>
+
+<content>
+  {scanResult.datetime}
+  {scanResult.text}
+  {scanResult.format}
+  {scanResult}</content
+>
+
+<style>
+</style>
