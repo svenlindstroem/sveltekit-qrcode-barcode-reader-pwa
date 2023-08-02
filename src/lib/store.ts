8b80c05a00f7871f1014d11df91bf18b165b6438
Sven Lindstroem <sven.lindstroem@gmail.com>
2023-08-02 14:14:13 +0200

    adding dervide store IsScanningDerived

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,9 +1,13 @@
-import { writable } from 'svelte/store'
+import { writable, derived } from 'svelte/store'
 
 const obj = {
   isOpenMenu: false,
   isScanning: false,
-  isDarkMode: false
+  isDarkMode: false,
+  hasMultipleCameras: false,
+  defaultCamera: false
 }
 
 export const appstate = writable(obj)
+
+export const isScanningDerived = derived(appstate, ($appstate) => $appstate.isScanning)
