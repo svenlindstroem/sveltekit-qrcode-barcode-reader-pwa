ec4825aa1894e13331849ff15831f87b0913b1bf
Sven Lindstroem <sven.lindstroem@gmail.com>
2023-08-04 12:55:50 +0200

    fixing typescript errors

--- src/lib/clickOutside.ts
+++ src/lib/clickOutside.ts
@@ -1,11 +1,14 @@
 /** Dispatch event on click outside of node */
-export function clickOutside(node) {
-  const handleClick = (event) => {
+export function clickOutside(node: HTMLElement) {
+  const handleClick = (event: MouseEvent) => {
     // ignore clicks on the menu or burger button
-    const el = event.target.closest('.ignore-clickoutside')
+    const target = event.target
+    if (!(target instanceof HTMLElement)) return
+    const el = target.closest('.ignore-clickoutside')
     if (el) return
-    if (node && !node.contains(event.target) && !event.defaultPrevented) {
-      node.dispatchEvent(new CustomEvent('clickedOutside', node))
+
+    if (node && !node.contains(target) && !event.defaultPrevented) {
+      node.dispatchEvent(new CustomEvent('clickedOutside'))
     }
   }
 
